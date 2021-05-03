8566764040a816ed707c6674f389e2ad93a90c5e
Adrian <adrian_zinko@op.pl>
2021-05-03 19:32:54 +0200

    Add menu navigation

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+import Link from 'next/link';
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li><Link href="/"><a>home</a></Link></li>
+          <li><Link href="/contact"><a>contact</a></Link></li>
+        </ul>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
